472cae495d902f1775dea5324fa99e14a14e7e65
Javohir <javohirnazarov99@gmail.com>
2020-09-25 21:14:26 +0500

    Added Filtering articles feature

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
     return super(Category, self).save()
 
   def __str__(self):
-    return self.title
+    return self.title 
 
 class Article(models.Model):
   title = models.CharField(max_length=255, null=False, blank=False)
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Article, Comment
+from .models import Article, Comment, Category
+from django.db.models import Q
 
 def article_list(request):
   articles= Article.objects.all()
-  return render(request, 'blog/latest.html', {'articles':articles})
+  categories = Category.objects.all()
+  keyword = request.GET.get('keyword', None)
+  category = request.GET.get('category', None)
+  month = request.GET.get('month', None)
+
+  if keyword:
+    articles = articles.filter(Q(title__icontains=keyword) | Q(text__icontains=keyword))
+
+  if category:
+    articles = articles.filter(category__title=category)
+
+  if month:
+    month_y = month[:4]
+    month_m= month[5:]
+    articles = articles.filter(created_at__year = month_y, created_at__month=month_m )
+  ctx = {'articles':articles, 'categories' :categories}
+  return render(request, 'blog/latest.html', ctx)
 
 def detail(request, id, slug):
    article = get_object_or_404(Article, id=id)
    comments = Comment.objects.all().filter(article=article)
    ctx = {'article' : article, 'comments' : comments}
    return render(request, 'blog/detail.html', ctx)
+
